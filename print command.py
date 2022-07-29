751130e6c8d66ebb39e1c4944a95965dd8f6a6f6
humayun247021 <110194244+humayun247021@users.noreply.github.com>
2022-07-29 23:59:27 +0600

    Add files via upload

---
+++ print command.py
@@ -0,0 +1,8 @@
+#পাইথন এ কোন কিছুর আউটপুট এর জন্য print commant ব্যবহার করতে হয় ।
+#তার জন্য print() ব্যবহার করতে হবে
+#intizer অথবা float টাইপের ডাটার জন্য  print() ব্যবহার করলে হবে কিন্তু স্ট্রিং এর জন্য অবশ্যই print("") ব্যবহার করতে হবে
+#example:
+
+print(10)
+print(10.5)
+print("Md Humayun Kabir Sobuj")
